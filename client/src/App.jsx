4fe7c8d858f65c3cfa2a96d3d68c67ca7660a8b1
Md Sholayman <mdsholayman998@gmail.com>
2024-01-05 15:47:46 +0600

    creating zustand store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
 
 const App = () => {
     return (
         <div>
-            
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/"} element={ <HomePage /> } />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
