bad1d91fba9563d1f7e9c3e701fe8663f0afade5
Md Sholayman <mdsholayman998@gmail.com>
2024-01-07 23:39:18 +0600

    navbar added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from "../components/layout/Layout.jsx";
 
 const HomePage = () => {
     return (
-        <div>
+        <Layout>
             
-        </div>
+        </Layout>
     );
 };
 
