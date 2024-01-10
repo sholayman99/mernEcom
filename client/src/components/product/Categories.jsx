669cd44af22eb901ef9806a5adf0b49aa1ddeb1d
Md Sholayman <mdsholayman998@gmail.com>
2024-01-10 17:26:18 +0600

    slider added

--- client/src/components/product/Categories.jsx
+++ client/src/components/product/Categories.jsx
@@ -15,19 +15,20 @@ const Categories = () => {
             <div className="section">
                 <div className="container">
                     <div className="row">
-                        <h1 className="headline-4 text-center my-2 p-0">Top Categories</h1>
-                        <span className="bodySmal mb-5 text-center">Explore a World of Choices Across Our Most Popular <br
+                     <h1 className="headline-4 text-center my-2 p-0">Top Categories</h1>
+                     <span className="bodySmal mb-5 text-center">Explore a World of Choices Across Our Most Popular <br
                         />Shopping Categories </span>
-                        {
-                            CategoryList.map((item,i)=> <div key={i} className="col-6 col-lg-8r text-center col-md-8r p-2">
-                                <Link to="" className="card h-100 rounded-3 bg-light">
-                                    <div className="card-body">
-                                        <img alt="" className="w-75" src={item['categoryImg']} />
-                                        <p className="bodySmal mt-3">{item['categoryName']}</p>
-                                    </div>
-                                </Link>
-                            </div> )
-                        }
+                {
+                    CategoryList.map((item,i)=> <div key={i} className="col-6 col-lg-8r text-center col-md-8r p-2">
+                        <Link to="" className="card h-100 rounded-3 bg-white">
+                            <div className="card-body">
+                                <img alt="" className="w-75" src={item['categoryImg']} />
+                                <p className="bodySmal mt-3">{item['categoryName']}</p>
+                            </div>
+                        </Link>
+                    </div> )
+                }
+
                     </div>
                 </div>
             </div>
--- client/src/components/product/Slider.jsx
+++ client/src/components/product/Slider.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProductStore from "../../store/ProductStore.js";
 import FeatureSkeleton from "../../skeleton/Feature-Skeleton.jsx";
 import SliderSkeleton from "../../skeleton/Slider-Skeleton.jsx";
+import {Link} from "react-router-dom";
 
 const Slider = () => {
 
@@ -13,6 +14,53 @@ const Slider = () => {
     else {
         return (
             <div>
+
+              <div id="carouselExampleDark" className="carousel hero-bg carousel-dark slide">
+                  <div className="carousel-indicators">
+                      {
+                       SliderList.map((item,i)=>{
+                        return(
+                            <button key={i} type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to={i}
+                            className="active" aria-current="true" aria-label=""></button>
+                        )
+                       })
+                      }
+                  </div>
+                    <div className="carousel-inner py-5">
+                  {
+                      SliderList.map((item,i)=> {
+                          let active = "carousel-item"
+                          if(i===0){
+                              active = "carousel-item active"
+                          }
+                          return(
+                           <div key={i} className={active} data-bs-interval="10000">
+                              <div className="container">
+                                 <div className="row justify-content-center">
+                                    <div className="col-12 col-lg-5 col-sm-12 col-md-5 p-5">
+                                       <h1 className="headline-1">{item['title']}</h1>
+                                       <p>{item['des']}</p>
+                                       <Link to="" className="btn text-white btn-success px-5">Buy Now</Link>
+                                    </div>
+                                    <div className="col-12 col-lg-5 col-sm-12 col-md-5 p-5">
+                                        <img src={item['image']} className="w-100" alt="..." />
+                                    </div>
+                                    </div>
+                                   </div>
+                                 </div>
+                                )
+                            })
+                  }
+                    </div>
+                    <button className="carousel-control-prev btn rounded-5" type="button" data-bstarget="#carouselExampleDark" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next btn" type="button" data-bs-target="#carouselExampleDark" databs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
 
             </div>
         );
